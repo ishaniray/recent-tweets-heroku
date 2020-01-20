057fbd7dae6ff799ec807def97315c1fbb1182f7
Ishani Ray <ishani_ray@yahoo.co.in>
2020-01-20 23:43:23 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// get these keys in https://apps.twitter.com/app/new 
-
-module.exports = {
-  consumer_key: 'VxZAcVnUEHisSc54qAH69LtI6',
-  consumer_secret: 'ZRL493TWMRoTB2ANwpi3Ve4JxdKQJC8qPtEolnYFM47ctQX25o',
-  access_token_key: '1217407804522188800-wivStXMlu1fvZpwIzJ7pLoua4VhSaD',
-  access_token_secret: 'DV6et6D0CRCx8a7ln5DMX7rDIlmB8t5P6PONblCW9FNjh'
-}

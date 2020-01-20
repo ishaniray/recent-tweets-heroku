c8d4e009b901478ef837c558978a86fccc5c57ae
Ishani Ray <ishani_ray@yahoo.co.in>
2020-01-20 22:56:51 +0530

    Handled async execution + minor refactoring

--- server.js
+++ server.js
@@ -6,60 +6,56 @@ const Twitter = require('twitter');
 const config = require('./config.js');
 const T = new Twitter(config);
 
-// Set up your search parameters
-const params = {
-    q: '#cerner',
+const searchParams = {
+    q: '#Cerner',
     count: 10,
-    result_type: 'recent', // this can be 'popular' or 'mixed'
+    result_type: 'recent',  // this can be 'popular' or 'mixed'
     lang: 'en'
-}
+};
 
 app.get('/', function(req, res) {
-    var tweetsUrls = [];
-    var embeddedTweets = [];
-    // Initiate your search using the above paramaters
-    T.get('search/tweets', params, (err, data, response) => {
-		// If there is no error, proceed
-        if(err){
+    
+    T.get('search/tweets', searchParams, (err, data, response) => {
+		// In case of an error, return
+        if(err) {
             return console.log(err);
         }
 
-        // Loop through the returned tweets
-        const tweetsId = data.statuses
-            .map(tweet => ({ id: tweet.id_str }));
-        
+        // Loop through the returned tweets and extract relevant information
+        const tweets = data.statuses.map(tweet => ({ 
+            id: tweet.id_str,
+            username: tweet.user.screen_name
+        }));
+
+        var oembedParams = {};
+        var embeddedTweets = [];
+        var count = 0;   
             
-        const tweetsScreenName = data.statuses
-            .map(tweet => ({ screenName: tweet.user.screen_name }));
-            
-        for(var i=0; i<tweetsId.length;++i) {
-            var id = tweetsId[i].id;
-            var username = tweetsScreenName[i].screenName;
-            var buildUrl = 'https://twitter.com/'+username+'/status/'+id;
-            var inputParams = {
-                url: 'test'
-            };
-            inputParams.url = buildUrl;
-            var put;
-            tweetsUrls.push(buildUrl);
-            put = T.get('statuses/oembed', inputParams , (err, data, response) => {
-                if(err){
+        for(var i = 0; i < tweets.length; ++i) {        
+            var id = tweets[i].id;
+            var username = tweets[i].username;
+            var fullUrl = `https://twitter.com/${username}/status/${id}`;
+            oembedParams.url = fullUrl;
+
+            T.get('statuses/oembed', oembedParams , (err, oembedData, response) => {
+                count = count + 1;
+
+                if(err) {
                     return console.log(err);
                 }
-                return data.html;
-                
+
+                embeddedTweets.push(oembedData.html);
+
+                if(count == tweets.length) {  // render index.ejs only when all callbacks but the current one have finished executing 
+                    res.render('index.ejs', {
+                        embeddedTweets: embeddedTweets
+                    });
+                }
             });
-            embeddedTweets.push(put);
         }
-        
-        console.log(embeddedTweets);
-		res.render('index.ejs',{
-			embeddedTweets: embeddedTweets
-		});
-        })
+    });
 });
 
 const server = http.listen(8080, function() {
     console.log('listening on *:8080');
 });
-

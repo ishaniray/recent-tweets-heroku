6d1bf23797fb187d069748e7fdf4b39ed5cd8dc2
Rohit Singh <c.ronnie.r@gmail.com>
2020-01-22 14:12:12 +0530

    Added Search Feature
    1. Added search #tag feature
    2. Generated dynamic header

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const apiKeys = {
 const T = new Twitter(apiKeys);
 
 const searchParams = {
-    q: '#Cerner',
+    q: '',
     count: 10,
     result_type: 'recent',  // this can be 'popular' or 'mixed'
     lang: 'en'
 };
 
 app.get('/', function(req, res) {
-
+    searchParams.q = '#Cerner';
     T.get('search/tweets', searchParams, (err, data, response) => {
 		// In case of an error, return
         if(err) {
@@ -55,12 +55,59 @@ app.get('/', function(req, res) {
 
                 if(count == tweets.length) {  // render index.ejs only when all callbacks but the current one have finished executing 
                     res.render('index.ejs', {
-                        embeddedTweets: embeddedTweets
+                        embeddedTweets: embeddedTweets,
+                        searchParams: searchParams
                     });
                 }
             });
         }
     });
+});
+
+app.get('/:id', function(req, res) {
+    searchParams.q = "#" + req.params.id;
+    T.get('search/tweets', searchParams, (err, data, response) => {
+		// In case of an error, return
+        if(err) {
+            return console.log(err);
+        }
+
+        // Loop through the returned tweets and extract relevant information
+        const tweets = data.statuses.map(tweet => ({ 
+            id: tweet.id_str,
+            username: tweet.user.screen_name
+        }));
+
+        var oembedParams = {};
+        var embeddedTweets = [];
+        var count = 0;   
+            
+        for(var i = 0; i < tweets.length; ++i) {        
+            var id = tweets[i].id;
+            var username = tweets[i].username;
+            var fullUrl = `https://twitter.com/${username}/status/${id}`;
+            oembedParams.url = fullUrl;
+
+            T.get('statuses/oembed', oembedParams , (err, oembedData, response) => {
+                count = count + 1;
+
+                if(err) {
+                    return console.log(err);
+                }
+
+                embeddedTweets.push(oembedData.html);
+
+                if(count == tweets.length) {  // render index.ejs only when all callbacks but the current one have finished executing 
+                    res.render('index.ejs', {
+                        embeddedTweets: embeddedTweets,
+                        searchParams: searchParams
+                    });
+                }
+            });
+        }
+    });
+    
+
 });
 
 const server = http.listen(8080, function() {

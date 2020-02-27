96980750494c8e745d600e32c93198c80148d2a8
Ishani Ray <ishani_ray@yahoo.co.in>
2020-02-27 14:35:04 +0530

    Adding comment to TableCreation.sql

--- resources/sql/TableCreation.sql
+++ resources/sql/TableCreation.sql
@@ -2,11 +2,11 @@ create table if not exists SearchedTerms (
     Id int primary key auto_increment,
     Hashtag varchar(255) not null,
     ResultType varchar(255) not null,
-    SearchedAt timestamp not null
+    SearchedAt timestamp not null -- On AWS RDS, this will be in UTC
 );
 
 create table if not exists UserRatings (
     Id int primary key auto_increment,
     Rating int not null,
-    PostedAt timestamp not null
+    PostedAt timestamp not null -- On AWS RDS, this will be in UTC
 );
